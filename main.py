ad79a848942bda805ecd29c71407b98f88684ef6
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 00:43:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ while (1):
             ten = st.text_input("Nhập tên:")
             tuoi = st.text_input("Nhập tuổi:")
             submitted = st.form_submit_button("Enter")
-            if submitted:
-                st.write('dfdsf')
+            if submitted: pass
+            st.write('dfdsf')
     except: pass
 
