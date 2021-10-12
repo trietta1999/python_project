516d4326ea513e35c316b5401d36257adfa788f7
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 03:11:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,10 +20,7 @@ if btn:
     st.write("Bạn tên là %s, %s tuổi" % (ten, tuoi))
     data_file = st.file_uploader("Upload", type=['jpg'])
     if data_file is not None:
-        #file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
-        #st.write(file_details)
-        #time.sleep(5)
-        #f = open(data_file,"r")
+        file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
         st.image(load_image(data_file),width=250)
     #st.info("Kết thúc chương trình")
     #st.info("Nhấn vào ≡ -> Rerun để chạy lại")
