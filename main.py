c2d6c5778aeaa0f8ca53fdce7d223602844df030
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 00:52:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 btn = st.button("Enter")
-
+st.write(btn)
 while (1):
     try:
         ten = st.text_input("Nhập tên:")
@@ -9,6 +9,6 @@ while (1):
         #submitted = st.form_submit_button("Enter")
         #if btn: st.write('on')
         #else: st.write('off')
-        st.write(btn)
+        
     except: pass
 
