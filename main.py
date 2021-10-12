0a4e506beb36e5a92cabe41e36b3035459effc8b
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 03:05:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ btn = st.button("Enter")
 if btn:
     st.write("Bạn tên là %s, %s tuổi" % (ten, tuoi))
     data_file = st.file_uploader("Upload")
-    #if data_file is not None:
-    #file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
-    #st.write(file_details)
-    #time.sleep(5)
-    f = open(data_file,"r")
-    st.write(f.read())
+    if data_file is not None:
+        #file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
+        #st.write(file_details)
+        #time.sleep(5)
+        #f = open(data_file,"r")
+        st.write(data_file.read())
     #st.info("Kết thúc chương trình")
     #st.info("Nhấn vào ≡ -> Rerun để chạy lại")
