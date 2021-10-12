8d913ac373db725a19daa9897c8a7735932f9fcf
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 02:26:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import os
 ten = st.text_input("Nhập tên:")
 tuoi = st.text_input("Nhập tuổi:")
 
-col1, col2,col3 = st.columns(3)
+'''
+col1, col2, col3 = st.columns(3)
+btn1 = col1.button("Enter1")
+btn2 = col2.button("Enter2")
+btn3 = col3.button("Enter3")
+'''
 
-with col1: btn = st.button("Enter")
-with col2: btn2 = st.button("Enter2")
-with col3: btn3 = st.button("Enter3")
+btn = st.button("Enter")
 
 if btn:
     st.write("Bạn tên là %s, %s tuổi" % (ten, tuoi))
