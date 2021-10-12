a385a56cddf9000d32c57227c5b394f066d6f1cd
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 01:22:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import streamlit as st
 
 while (1):
     try:
-        ten = st.text_input("Nhập tên:")
-        tuoi = st.text_input("Nhập tuổi:")
-        #btn = st.button("Enter")
-        #st.text(btn)
-        btn = st.form_submit_button("Enter")
-        if btn: st.write('on')
-        else: st.write('off')
+        with st.form("dfsd"):
+            ten = st.text_input("Nhập tên:")
+            tuoi = st.text_input("Nhập tuổi:")
+            #btn = st.button("Enter")
+            #st.text(btn)
+            btn = st.form_submit_button("Enter")
+            if btn: st.write('on')
+            else: st.write('off')
         
     except: pass
