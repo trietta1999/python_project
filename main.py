b62ffedb1493db3839cf7a5aca2c81ae5a998d19
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 02:54:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ btn = st.button("Enter")
 
 if btn:
     st.write("Bạn tên là %s, %s tuổi" % (ten, tuoi))
-    data_file = st.file_uploader("Upload")
+    data_file = st.file_uploader("Upload",type=['exe'])
     if data_file is not None:
         file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
         st.write(file_details)
