76f5a990846c9a4dbde07d8970da8f63d0b8e26e
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 02:23:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import os
 ten = st.text_input("Nhập tên:")
 tuoi = st.text_input("Nhập tuổi:")
 
-col1, col2 = st.rows(2)
+col1, col2 = st.columns(2)
 
-btn = col1.button("Enter")
-btn2 = col1.button("Enter2")
+with col1: btn = button("Enter")
+with col2: btn2 = col1.button("Enter2")
 
 if btn:
     st.write("Bạn tên là %s, %s tuổi" % (ten, tuoi))
