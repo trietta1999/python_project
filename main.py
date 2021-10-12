0ec00bac67728535f2ccff0281f6bbbccdde9d3f
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 01:55:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ tuoi = st.text_input("Nhập tuổi:")
 btn = st.button("Enter")
 
 if btn:
-    st.write("Bạn tên là ",ten,", ",tuoi," tuổi")
+    st.write("Bạn tên là ",ten,", ",tuoi," tuổi", sep='')
 #else: st.write('off')
