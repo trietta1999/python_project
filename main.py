cdef86078b7a577db933393042017c5d834eb22a
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 00:29:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
-try:
-  ten = st.text_input("Nhập tên:")
-  tuoi = st.text_input("Nhập tuổi:")
-  print("Bạn",ten," dễ thương quá,",tuoi," rồi mà còn trẻ lắm!")
-except: pass
+while (1):
+  try:
+    ten = st.text_input("Nhập tên:")
+    tuoi = st.text_input("Nhập tuổi:")
+    print("Bạn",ten," dễ thương quá,",tuoi," rồi mà còn trẻ lắm!")
+  except: pass
 
