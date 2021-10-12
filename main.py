e5362cf7a958d193776a06fca41670a3ccd878a1
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 01:25:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ while (1):
     try:
         ten = st.text_input("Nhập tên:")
         tuoi = st.text_input("Nhập tuổi:")
+    except: pass
         btn = st.button("Enter")
         #st.text(btn)
         #btn = st.form_submit_button("Enter")
         if btn: st.write('on')
         else: st.write('off')
-        
-    except: pass
