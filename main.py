50074390d6e159fbd04259b9dacafcd6ef9f12c6
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 00:26:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import streamlit as st
-a=""
+
 while(1):
-  a = st.text_input("label goes here:")
-  st.write(a)
-  st.latex("\displaystyle\sum_{i=1}^{10} t_i")
+  try:
+    a = st.text_input("label goes here:")
+    st.write(a)
+    st.latex("\displaystyle\sum_{i=1}^{10} t_i")
+  except: pass
 
