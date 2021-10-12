0692ad6a6076b23379471078b5ef6b799ebea9ca
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 01:45:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 import streamlit as st
 
-@st.cache(allow_output_mutation=True)
-def button_states():
-    return {"pressed": None}
-
 ten = st.text_input("Nhập tên:")
 tuoi = st.text_input("Nhập tuổi:")
 btn = st.button("Enter")
 
-while (1):
-
-    is_pressed = button_states()
-    if btn: is_pressed.update({"pressed": True})
-    if is_pressed["pressed"]:
-        st.write('on')
-        is_pressed = button_states()
-    #else: st.write('off')
+if btn:
+    st.write('on')
+else: st.write('off')
