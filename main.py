99c0b8ddca949f068a214b9d56661d6adc7bccd5
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 01:26:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ while (1):
         ten = st.text_input("Nhập tên:")
         tuoi = st.text_input("Nhập tuổi:")
     except: pass
-        btn = st.button("Enter")
-        #st.text(btn)
-        #btn = st.form_submit_button("Enter")
-        if btn: st.write('on')
-        else: st.write('off')
+    btn = st.button("Enter")
+    #st.text(btn)
+    #btn = st.form_submit_button("Enter")
+    if btn: st.write('on')
+    else: st.write('off')
