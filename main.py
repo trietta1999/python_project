f435166325d6fafd71c57629743d2f680d3f7aa0
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 02:20:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import os
 ten = st.text_input("Nhập tên:")
 tuoi = st.text_input("Nhập tuổi:")
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns(1)
 
 btn = col1.button("Enter")
 btn2 = col2.button("Enter2")
 
 if btn:
     st.write("Bạn tên là %s, %s tuổi" % (ten, tuoi))
-st.write("Kết thúc chương trình")
-st.write("Nhấn vào ⫼ -> Rerun để chạy lại")
+    st.write("Kết thúc chương trình")
+    st.write("Nhấn vào ⫼ -> Rerun để chạy lại")
