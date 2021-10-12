db3bff397fd14b9dbe4ac9384324b48a7aa75f7b
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 00:38:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import streamlit as st
 
 while (1):
     try:
-        ten = st.text_input("Nhập tên:")
-        tuoi = st.text_input("Nhập tuổi:")
-        submitted = st.form_submit_button("Enter")
-        if submitted:
-            st.write("Bạn" + ten + " dễ thương quá," + tuoi + " rồi mà còn trẻ lắm!")
+        with st.form("Test"):
+            ten = st.text_input("Nhập tên:")
+            tuoi = st.text_input("Nhập tuổi:")
+            submitted = st.form_submit_button("Enter")
+            if submitted:
+                st.write("Bạn" + ten + " dễ thương quá," + tuoi + " rồi mà còn trẻ lắm!")
     except: pass
 
