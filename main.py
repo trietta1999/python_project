b3c8eb3de52ff9a0005382d02110f5db3ba801c6
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 00:30:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ while (1):
   try:
     ten = st.text_input("Nhập tên:")
     tuoi = st.text_input("Nhập tuổi:")
-    pst.write("Bạn",ten," dễ thương quá,",tuoi," rồi mà còn trẻ lắm!")
+    st.write("Bạn",ten," dễ thương quá,",tuoi," rồi mà còn trẻ lắm!")
   except: pass
 
