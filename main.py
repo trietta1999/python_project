3db99107ef8e4b4e2c3f9519a46ab412db9089ae
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 04:01:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import streamlit as st
 from PIL import Image
 from io import BytesIO, StringIO
+from typing import Dict
 import os, time
+
+@st.cache(allow_output_mutation=True)
+def get_static_store() -> Dict:
+	"""This dictionary is initialized once and can be used to store the files uploaded"""
+	return {}
 
 def load_image(image_file):
 	img = Image.open(image_file)
@@ -15,10 +21,18 @@ btn = st.button("Enter")
 # btn1 = col1.button("Enter1")
 # btn2 = col2.button("Enter2")
 # btn3 = col3.button("Enter3")
+static_store = get_static_store()
 
 if btn:
 	st.write("Bạn tên là %s, %s tuổi" % (ten, tuoi))
-	#data_file = st.file_uploader("Upload")
-	#if data_file:
+	data_file = st.file_uploader("Upload")
+	if data_file:
+		c = data_file.getvalue()
+		st.write(c)
+		if not c in static_store.values():
+			static_store[data_file] = data_file
+	else:
+		static_store.clear()  # Hack to clear list if the user clears the cache and reloads the page
+		st.info("Upload one or more `.py` files.")
 	#	st.write(type(data_file))
 	#else: st.write("None")
