37de3349eb2fc59da7184a468afeb392bf28c558
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 02:00:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import streamlit as st
-import os
+import os, pyautogui
 
-os.system("pip uninstall pyqt5")
-os.system("pip install pyautogui")
+#os.system("pip uninstall pyqt5")
+#os.system("pip install pyautogui")
 #os.system("python --version")
 
 ten = st.text_input("Nhập tên:")
 tuoi = st.text_input("Nhập tuổi:")
 btn = st.button("Enter")
+btn2 = st.button("Chạy lại")
 
 if btn:
     st.write("Bạn tên là %s, %s tuổi" % (ten, tuoi))
-#else: st.write('off')
+st.write("Kết thúc chương trình")
+
+if btn2:
+    pyautogui.press('R')
