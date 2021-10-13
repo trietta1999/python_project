4dd9a478f03c231eedf9b63e7e0e3536e3a6dad8
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 19:11:37 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,11 +2,12 @@ import streamlit as st
 import requests
 import pandas as pd
 
-txt = requests.get('https://archive.org/download/test_20211013_202110/test.csv').content
-st.write(txt)
+txt = requests.get('https://archive.org/download/lumoria_b/lumoria_b.map').content
+#st.write(txt)
 
-with open('test.csv','wb') as f:
-  f.write(txt)
+#with open('test.map','wb') as f:
+#  f.write(txt)
 
-df = pd.read_csv('test.csv')
-st.write(df['xmin'])
+#df = pd.read_csv('test.csv')
+#st.write(df['xmin'])
+st.download_button('Download binary file', txt)
