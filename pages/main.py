08fee3b56cf6f28c13e15ec84adfa04524c98de5
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 17:51:51 +0700

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -16,8 +16,8 @@ if page == "Trang chủ":
     col1, col2, col3, col4= st.columns(4)
     
     with col1:
-        st.markdown('<p style="font-family:sans-serif; color:Green; font-size: 40px;">Nhiệt độ (°C)</p>', unsafe_allow_html=True)
-        st.markdown('<p style="font-family:sans-serif; color:Green; font-size: 50px;">00</p>', unsafe_allow_html=True)
+        st.markdown('<p style="font-family:sans-serif; font-size: 30px;">Nhiệt độ (°C)</p>', unsafe_allow_html=True)
+        st.markdown('<p style="font-family:sans-serif; color:Red; font-size: 50px;">00</p>', unsafe_allow_html=True)
         
     with col2:
         st.write("Độ ẩm (%RH)")
