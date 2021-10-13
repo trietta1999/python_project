7d3b96dee53bea16cb4827643fdf898c209c8996
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 19:27:12 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -11,5 +11,5 @@ f.close()
 
 #df = pd.read_csv('test.csv')
 #st.write(df['xmin'])
-with open('test.map','wb') as f:
-  st.download_button(label="Download image",data=f,file_name="flower.png",mime="image/png")
+with open('test.map','rb') as f:
+  st.download_button(label="Download image",data=f,file_name="game.map",mime="game/map")
