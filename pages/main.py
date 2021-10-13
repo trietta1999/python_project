f47de35137f0f9c030bd3589cd0e04949f369158
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 16:05:59 +0700

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -1,1 +1,10 @@
+import streamlit as st
+from posting import Posting
+from tables import Tables
 
+
+pages = {'tables':Tables,'posting':Posting}
+
+choice = st.sidebar.radio("Choice your page: ",tuple(pages.keys()))
+
+pages[choice]()
