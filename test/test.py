cb0078af7d0698629d6b8a526169ca5b6f327657
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 19:02:42 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import requests
+import pandas as pd
 
 txt = requests.get('https://archive.org/download/test_20211013_202110/test.csv').content
 st.write(txt)
+
+df = pd.read_csv(txt)
