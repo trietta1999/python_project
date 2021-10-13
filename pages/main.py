9db10897628cabf794c6bfc966c190f7c15068c6
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 16:28:01 +0700

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -3,9 +3,7 @@ import datetime, time
 
 page = st.selectbox("Choose your page", ["Page 1", "Page 2", "Page 3"])
 if page == "Page 1":
-    #st.write(datetime.datetime.now())
-    while (1):
-        st.text_on_same_line(datetime.datetime.now())  # need to incrementally display a new word at every step
+    st.write(datetime.datetime.now(), end="")
 elif page == "Page 2":
     st.write("Display details of page 2")
     b = st.button("Button")
