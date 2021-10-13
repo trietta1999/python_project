f8e816151465df0857e8cc8c857b4fd837937b00
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 16:18:42 +0700

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -3,7 +3,7 @@ import datetime
 
 page = st.selectbox("Choose your page", ["Page 1", "Page 2", "Page 3"])
 if page == "Page 1":
-    st.write(datetime.datetime.now())
+    while (1): st.write(datetime.datetime.now())
 elif page == "Page 2":
     st.write("Display details of page 2")
     b = st.button("Button")
