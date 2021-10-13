7890f841f1e7a68fbd3895bfc2325773f71032d1
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 18:34:21 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,4 +2,4 @@ import streamlit as st
 import requests
 
 txt = requests.get('https://colab.research.google.com/drive/1ksuBH5IVPBuR7Mxz96rxeSuE0hHLO-q1?usp=sharing', stream=True).raw
-st.write(txt)
+st.write(txt.getvalue())
