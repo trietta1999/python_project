8c539a37a495ded2c38b1858c7b437194d09b0c1
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 21:43:46 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,15 +1,18 @@
 import streamlit as st
-import requests
+import requests, os
 import pandas as pd
 
-txt = requests.get('https://archive.org/download/lumoria_b/lumoria_b.map').content
-#st.write(txt)
+if (os.path.exists('test.jpg')==False):
+  st.write("false")
+  txt = requests.get('https://archive.org/download/mbid-a212af05-ac1c-478c-a8bf-9621681c13e6/mbid-a212af05-ac1c-478c-a8bf-9621681c13e6-30432369209_itemimage.jpg').content
+  #st.write(txt)
 
-with open('test.map','wb') as f:
-  st.write(f.write(txt))
-f.close()
+  with open('test.jpg','wb') as f:
+    st.write(f.write(txt))
+    f.close()
+else: st.write("true")
 
 #df = pd.read_csv('test.csv')
 #st.write(df['xmin'])
-with open('test.map','rb') as f:
-  st.download_button(label="Download image",data=f,file_name="game.map",mime="game/map")
+#with open('test.map','rb') as f:
+#  st.download_button(label="Download image",data=f,file_name="game.map",mime="game/map")
