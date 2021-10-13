57396f315a45b367849b61596107dad084238fcb
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 17:34:24 +0700

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -7,13 +7,23 @@ def rerun(t):
     raise st.script_runner.RerunException(st.script_request_queue.RerunData(None))
 
 page = st.selectbox("", ["Trang chủ", "Điều khiển", "Giám sát", "Thống kê"])
+
 if page == "Trang chủ":
     st.write(datetime.datetime.now(pytz.timezone('Asia/Saigon')).strftime("%a %d/%m/%Y, %X"))
     image = Image.open(requests.get('https://hotondo.com.au/wp-content/uploads/2016/06/Header-1.jpg', stream=True).raw)
     st.image(image, caption='')
+    
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        st.header("Nhiệt độ (°C)")
+        b_nhietdo = st.button("00")
+    
     rerun(1)
+
 elif page == "Page 2":
     st.write("Display details of page 2")
     b = st.button("Button")
+
 elif page == "Page 3":
     st.write("Display details of page 3")
