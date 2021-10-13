5d5319aea6472216ecc7cdd21acd78b1aedb8165
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 16:35:08 +0700

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -1,10 +1,15 @@
 import streamlit as st
-import datetime, time
+import datetime, time, threading
 
+def time_update():
+    while (1):
+        st.write(datetime.datetime.now())
+        time.sleep(1)
+        
 page = st.selectbox("Choose your page", ["Page 1", "Page 2", "Page 3"])
 if page == "Page 1":
-    st.write(datetime.datetime.now())
-    st.rerun()
+    t = threading.Thread(target=time_update)
+    t = start()
 elif page == "Page 2":
     st.write("Display details of page 2")
     b = st.button("Button")
