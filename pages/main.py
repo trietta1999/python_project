50796425b059936e1f89c227336850f8fd6543b1
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 16:18:11 +0700

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -1,8 +1,9 @@
-import streamlit as st #Create a page dropdown 
+import streamlit as st
+import datetime
+
 page = st.selectbox("Choose your page", ["Page 1", "Page 2", "Page 3"])
 if page == "Page 1":
-    st.write("Display details of page 1")
-    a = st.text_input("Input")
+    st.write(datetime.datetime.now())
 elif page == "Page 2":
     st.write("Display details of page 2")
     b = st.button("Button")
