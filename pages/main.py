1d40b47c0f4b58128f9641e0e77336392ff81e17
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 16:21:50 +0700

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -1,10 +1,14 @@
 import streamlit as st
-import datetime
+import datetime, time
 
 page = st.selectbox("Choose your page", ["Page 1", "Page 2", "Page 3"])
 if page == "Page 1":
-    #while (1):
-    st.write(datetime.datetime.now())
+    #st.write(datetime.datetime.now())
+    while (1):
+        t = st.empty()
+        for i in range(len(datetime.datetime.now()) + 1):
+            t.markdown("## %s..." % text[0:i])
+            time.sleep(0.1)
 elif page == "Page 2":
     st.write("Display details of page 2")
     b = st.button("Button")
