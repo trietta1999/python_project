eb07669de59d321c50606b36259e00f1c3597f47
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 16:11:39 +0700

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -1,8 +1,8 @@
 import streamlit as st# Create a page dropdown 
 page = st.selectbox("Choose your page", ["Page 1", "Page 2", "Page 3"])
 if page == "Page 1":
-    # Display details of page 1
+    st.wite("Display details of page 1")
 elif page == "Page 2":
-    # Display details of page 2
+    st.wite("Display details of page 2")
 elif page == "Page 3":
-    # Display details of page 3
+    st.wite("Display details of page 3")
