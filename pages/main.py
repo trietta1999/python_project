64b99e814167e8e83c93d13802119edea072ff5b
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 16:06:32 +0700

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -1,10 +1,48 @@
+import os
+import pickle
 import streamlit as st
-from posting import Posting
-from tables import Tables
 
+class State:
+    def __init__(self, path='state.pickle', default_state_class=dict):
+        self.path = path
+        self.default_state_class = default_state_class
 
-pages = {'tables':Tables,'posting':Posting}
+    def load(self):
+        if os.path.exists(self.path):
+            with open(self.path, 'rb') as inf:
+                self.state = pickle.load(inf)
+        else:
+            self.state = self.default_state_class()
 
-choice = st.sidebar.radio("Choice your page: ",tuple(pages.keys()))
+    def get_state(self):
+        return self.state
 
-pages[choice]()
+    def save(self):
+        with open(self.path, 'wb') as outf:
+            pickle.dump(self.state, outf)
+
+def rerun():
+    raise st.script_runner.RerunException(st.script_request_queue.RerunData(None))
+
+def app():
+    store = State()
+    store.load()
+
+    name = store.get_state().get('name', None)
+    if name:
+        st.text(f'Hello {name}')
+    else:
+        st.text(f'Please enter your name')
+        name_input = st.text_input('your name')
+        name = name_input
+
+        if name != '':
+            store.get_state()['name'] = name
+
+        store.save()
+        next_page = st.button('Next page')
+        if next_page:
+            rerun()
+
+if __name__ == "__main__":
+    app()
