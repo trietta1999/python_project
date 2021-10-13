6861e706b7c050f2b13aafa69a6ac6fce5d35ac0
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 18:47:35 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import requests
 
-txt = requests.get('https://archive.org/download/unnamed-asset-level-0-3234-mono-behaviour/unnamed%20asset-level0-3234-MonoBehaviour.txt')
+txt = requests.get('https://archive.org/download/unnamed-asset-level-0-3234-mono-behaviour/unnamed%20asset-level0-3234-MonoBehaviour.txt').text
 st.write(txt)
