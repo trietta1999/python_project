703cff44976bd5472471d4113464844fa9c484bf
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 16:22:16 +0700

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -6,7 +6,7 @@ if page == "Page 1":
     #st.write(datetime.datetime.now())
     while (1):
         t = st.empty()
-        for i in range(len(datetime.datetime.now()) + 1):
+        for i in range(len(str(datetime.datetime.now())) + 1):
             t.markdown("## %s..." % text[0:i])
             time.sleep(0.1)
 elif page == "Page 2":
