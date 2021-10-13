fad736504f7c9c7aa900c2a4b22cb1970beb5e12
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 18:37:36 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,4 +2,4 @@ import streamlit as st
 import requests
 
 txt = requests.get('https://drive.google.com/file/d/1ML1D8EfrIlI0Xf3CsdWzmkNf9twRX8od/view?usp=sharing', stream=True).raw
-st.write(txt.getvalue())
+st.write(txt)
