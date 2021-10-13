c5f6136c72db188b9c9de2101cc94e4ea0557817
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-13 18:59:14 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import requests
 
-txt = requests.get('https://archive.org/download/test_20211013_202110/test.csv').content
+txt = requests.get('https://archive.org/download/test_20211013_202110/test.csv',params=ploads).content
 st.write(txt)
