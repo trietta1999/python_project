66ba4ad5aa9262022f9b600f3a25f08ffa56ee2d
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-14 14:55:14 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,12 +2,12 @@ import os
 import subprocess
 import sys
 
-from src.config import EnvironmentalVariableNames as EnvVar, get_env
+#from src.config import EnvironmentalVariableNames as EnvVar, get_env
 
 def main():
     executable = sys.executable
     result = subprocess.run(
-        f"{executable} -m streamlit run {os.path.join(get_env(EnvVar.EMPORIO_VESTIARIO_DASHBOARD_WORKING_DIR), 'pages', 'main.py')}",
+        f"{executable} -m streamlit run {os.path.join('pages', 'main.py')}",
         shell=True,
         capture_output=True,
         text=True,
