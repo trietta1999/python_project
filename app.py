95dc65d7972c91a916942622a713e005909b355c
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-14 16:51:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import importlib
 
 st.write("DB username:", st.secrets["db_username"])
 st.write("DB password:", st.secrets["db_password"])
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
 
 # And the root-level secrets are also accessible as environment variables:
 
