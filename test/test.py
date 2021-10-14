2b4a9e9a095b4db021535c6fe201291bd164a795
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-14 14:52:22 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,6 +1,18 @@
-import subprocess, os
-import streamlit as st
+import os
+import subprocess
+import sys
 
-#st.write(subprocess.call(os.getcwd()))
-#st.write(subprocess.run([os.chdir("/app/python/pages"),"&&","streamlit", "run", 'main.py']))
-subprocess.Popen(["streamlit", "run",os.path.join("/app/python/pages/main.py")])
+from src.config import EnvironmentalVariableNames as EnvVar, get_env
+
+def main():
+    executable = sys.executable
+    result = subprocess.run(
+        f"{executable} -m streamlit run {os.path.join(get_env(EnvVar.EMPORIO_VESTIARIO_DASHBOARD_WORKING_DIR), 'pages', 'main.py')}",
+        shell=True,
+        capture_output=True,
+        text=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
