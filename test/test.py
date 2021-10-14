f7905443a990c26dcb67ad95d37ce6e8c3d675f1
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-14 15:02:20 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,18 +1,3 @@
-import os
-import subprocess
-import sys
+from pages import main
 
-#from src.config import EnvironmentalVariableNames as EnvVar, get_env
-
-def main():
-    executable = sys.executable
-    result = subprocess.run(
-        f"{executable} -m streamlit run {os.path.join('pages', 'main.py')}",
-        shell=True,
-        capture_output=True,
-        text=True,
-    )
-
-
-if __name__ == "__main__":
-    main()
+main.main()
