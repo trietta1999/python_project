270e0ad2987d83f031efdc76910e3b7cf424ac1a
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-14 15:54:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ import sys
 app = st.selectbox("", ["Smart Steward", "YoloV4"])
 
 if (app=="Smart Steward"):
+  sys.path.remove("pages") 
   sys.path.append("pages")  
   from main import main
   main()
   
 elif (app=="YoloV4"):
+  sys.path.remove("yolo")
   sys.path.append("yolo")  
   from detection import main
   main()
