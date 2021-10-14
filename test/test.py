94ea8a41d5db9b3ad270f592db8e57366bc83cff
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-14 14:45:54 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,5 +2,5 @@ import subprocess, os
 import streamlit as st
 
 #st.write(subprocess.call(os.getcwd()))
-st.write(subprocess.run([os.chdir("/app/python/pages"),"&&","streamlit", "run", 'main.py']))
-
+#st.write(subprocess.run([os.chdir("/app/python/pages"),"&&","streamlit", "run", 'main.py']))
+st.write(subprocess.run(["streamlit", "run",os.path.join("/app/python/pages/main.py")]))
