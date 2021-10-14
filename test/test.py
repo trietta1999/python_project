3556d7c3a873619a8ace56d0fea6d03adbbf1c2f
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-14 14:41:04 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 import subprocess, os
 import streamlit as st
 
-st.write(subprocess.run(["ls"]))
-st.write(subprocess.run([os.chdir("pages"),"&&","streamlit", "run", 'main.py']))
+st.write(subprocess.call(["ls"]))
+st.write(subprocess.call([os.chdir("pages"),"&&","streamlit", "run", 'main.py']))
 
