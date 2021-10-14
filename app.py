4b1c7ce2b11956b24be6acac52d7cfe5afeac26a
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-14 16:51:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ import streamlit as st
 import sys, os
 import importlib
 
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+
+
+st.write(
+		"Has environment variables been set:",
+		os.environ["db_username"] == st.secrets["db_username"])
+
 sys.path.append("pages")
 sys.path.append("yolo")
 
