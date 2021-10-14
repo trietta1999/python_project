658416d308c433aef688090cae4ccf7b4c34192c
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-14 15:13:57 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,10 +1,10 @@
 import streamlit as st
-import sys
-st.write(sys.path.insert(0, '/pages/'))
 
 import os
 os.system("ls")
 
-from pages import main
+import sys  
+sys.path.append("pages")  
+import main #scriptName without .py extension  
 
 main.main()
