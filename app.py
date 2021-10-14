435fce117c7c6cd44584964a25db718095acf04b
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-14 16:34:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 import streamlit as st
-import sys, os  
+import sys, os
+import importlib
 
-# sys.path.append("pages")
-# sys.path.append("yolo")  
+sys.path.append("pages")
+sys.path.append("yolo")
 
-# app = st.selectbox("", ["Smart Steward", "YoloV4"])
+app = st.selectbox("", ["Smart Steward", "YoloV4"])
 
-# if (app=="Smart Steward"):  
-#     from main import main
-#     main()
+if (app=="Smart Steward"):
+    importlib.reload(main)
+    from main import main
+    main()
     
-# elif (app=="YoloV4"):
-#     from detection import main
-#     main()
-
-os.system("cd yolo ; ls")
+elif (app=="YoloV4"):
+    importlib.reload(detection)
+    from detection import main
+    main()
