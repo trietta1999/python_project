1bd78f176a29d3969a0c804699302133fd6d71fd
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-14 14:22:36 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,6 +1,15 @@
-import streamlit as st
-import os
+import click, os
+import streamlit.cli
 
-os.system("cd pages & streamlit run main.py")
-#os.system("ls")
-#os.system("python main.py")
+@click.group()
+def main():
+    pass
+
+@main.command("streamlit")
+def main_streamlit():
+    filename = os.path.join("pages, 'main.py')
+    args = []
+    streamlit.cli._main_run(filename, args)
+
+if __name__ == "__main__":
+    main()
