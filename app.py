1aa80f31c46e3e87dcdc67a4851dc9115b0cc93c
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-14 16:07:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 import streamlit as st
 import sys  
 
-sys.path.remove("pages") 
-sys.path.remove("yolo")
 try:
-  del sys.modules["main", "detection"]
+    sys.path.remove("pages") 
+    sys.path.remove("yolo")
+    del sys.modules["main", "detection"]
 except: pass
+
 app = st.selectbox("", ["Smart Steward", "YoloV4"])
 
 if (app=="Smart Steward"):
-  sys.path.append("pages")  
-  from main import main
-  main()
+    sys.path.append("pages")  
+    from main import main
+    main()
   
 elif (app=="YoloV4"):
-  sys.path.append("yolo")  
-  from detection import main
-  main()
+    sys.path.append("yolo")  
+    from detection import main
+    main()
