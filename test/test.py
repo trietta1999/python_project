fc18437b06cbfe88b8b205f386a6ef70ee51e3bd
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-14 15:19:14 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,7 +1,14 @@
 import streamlit as st
-
 import sys  
-sys.path.append("pages")  
 
-import main
-main.main()
+app = st.selectbox("", ["Smart Steward", "YoloV4"])
+
+if (app=="Smart Steward"):
+  sys.path.append("pages")  
+  from main import main
+  main()
+  
+elif (app=="YoloV4"):
+  sys.path.append("yolo")  
+  from detection import main
+  main()
