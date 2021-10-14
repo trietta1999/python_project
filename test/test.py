a3378307de1437d0e79cea347be3b86b835f070e
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-14 14:16:00 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,22 +1,4 @@
 import streamlit as st
-import requests, os
-import pandas as pd
-from PIL import Image
+import os
 
-if (os.path.exists('test.jpg')==False):
-  st.write("false")
-  txt = requests.get('https://archive.org/download/mbid-a212af05-ac1c-478c-a8bf-9621681c13e6/mbid-a212af05-ac1c-478c-a8bf-9621681c13e6-30432369209_itemimage.jpg').content
-  #st.write(txt)
-
-  with open('test.jpg','wb') as f:
-    st.write(f.write(txt))
-    f.close()
-else:
-  st.write("true")
-  image = Image.open('test.jpg')
-  st.image(image, caption='Sunrise by the mountains')
-
-#df = pd.read_csv('test.csv')
-#st.write(df['xmin'])
-#with open('test.map','rb') as f:
-#  st.download_button(label="Download image",data=f,file_name="game.map",mime="game/map")
+os.system("python ../pages/main.py")
