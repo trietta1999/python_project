e7a01f8ba7db1bb611a85bc5dab4ee6325351502
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-21 16:17:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
 import streamlit as st
 import sys, os, importlib
-st.write(os.path.getsize("yolov4-custom_best.weights"))
-# sys.path.append("pages")
-# sys.path.append("yolo")
 
-# app = st.selectbox("Chọn App", ["Smart Steward", "YoloV4"])
+sys.path.append("pages")
+sys.path.append("yolo")
 
-# if (app=="Smart Steward"):
-#     if (os.path.exists("loginok.log")==False):
-#         ten = st.text_input("Tên đăng nhập:")
-#         mk = st.text_input("Mật khẩu")
-#         check = st.button("Đăng nhập")
+app = st.selectbox("Chọn App", ["Smart Steward", "YoloV4"])
 
-#         if check:
-#             if (ten==st.secrets["db_username"] and mk==st.secrets["db_password"]):
-#                 f = open("loginok.log","w")
-#                 f.close()
-#                 raise st.script_runner.RerunException(st.script_request_queue.RerunData(None))
-#             else: st.write("Tài khoản không đúng!")
-#     else:
-#         import pages
-#         importlib.reload(pages)
-#         pages.main()
-# elif (app=="YoloV4"):
-#     import detection
-#     importlib.reload(detection)
-#     detection.main()
+if (app=="Smart Steward"):
+    if (os.path.exists("loginok.log")==False):
+        ten = st.text_input("Tên đăng nhập:")
+        mk = st.text_input("Mật khẩu")
+        check = st.button("Đăng nhập")
+
+        if check:
+            if (ten==st.secrets["db_username"] and mk==st.secrets["db_password"]):
+                f = open("loginok.log","w")
+                f.close()
+                raise st.script_runner.RerunException(st.script_request_queue.RerunData(None))
+            else: st.write("Tài khoản không đúng!")
+    else:
+        import pages
+        importlib.reload(pages)
+        pages.main()
+elif (app=="YoloV4"):
+    import detection
+    importlib.reload(detection)
+    detection.main()
