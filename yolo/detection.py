c70daf93510b54699cdf90e59493aad21f8ba584
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2021-10-21 16:30:19 +0700

    Update detection.py

--- yolo/detection.py
+++ yolo/detection.py
@@ -14,7 +14,7 @@ nms_threshold = 0.4
 
 def get_output_layers(net):
     layer_names = net.getLayerNames()
-    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
     return output_layers
 
 def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
