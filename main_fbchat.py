3e204f08618f1b85d29c36559e55569993dc6ff6
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2022-01-08 19:06:06 +0700

    Update main_fbchat.py

--- main_fbchat.py
+++ main_fbchat.py
@@ -27,12 +27,25 @@ try:
 	client = EchoBot(st.secrets["fb_uname"], st.secrets["fb_pass"], code)
 except: pass
 			
-app = st.selectbox("Menu", ["Đăng ký tài khoản", "Kiểm thử chat"])
+app = st.selectbox("Menu", ["Đăng ký tài khoản", "Kiểm thử chat", "Bảo trì"])
 if (app=="Đăng ký tài khoản"):			
 	uname = st.text_input("UID:")
 	st.write("Lấy UID bằng link tài khoản FB tại đây: [findidfb.com](https://findidfb.com/)")
 	dk = st.button("Đăng ký")
 	
+	if dk:
+		st.warning("Bạn có muốn tiếp tục?")
+		col1, col2 = st.columns(2)
+		with col1:
+			tt = st.button("Tiếp tục")
+			if tt: client.send(Message(text="Cám ơn bạn đã đăng ký, bây giờ bạn có thể chat."), thread_id=uname, thread_type=ThreadType.USER)
+			
+		with col2:
+			huy = st.button("Hủy bỏ")
+			if huy: rerun()
+		
+elif (app=="Bảo trì"):
+		
 	col1, col2 = st.columns(2)
 	code = ''
 	with col1:
@@ -46,20 +59,7 @@ if (app=="Đăng ký tài khoản"):
 		check = st.button("Đăng nhập")
 	with col2:
 		st.error("Mục này chỉ dành cho nhà phát triển, nếu nhấn sẽ gây lỗi")
-	
-	if dk:
-		st.warning("Bạn có muốn tiếp tục?")
-		col1, col2 = st.columns(2)
-		with col1:
-			tt = st.button("Tiếp tục")
-			if tt: client.send(Message(text="Cám ơn bạn đã đăng ký, bây giờ bạn có thể chat."), thread_id=uname, thread_type=ThreadType.USER)
-			
-		with col2:
-			huy = st.button("Hủy bỏ")
-			if huy: rerun()
 		
 	if check:
 		client = EchoBot(st.secrets["fb_uname"], st.secrets["fb_pass"], code)	
-	
-#elif (app=="Kiểm thử chat"):
 		client.listen()
