5a412e62d3b315ca466c5577866dd4c7135f0f41
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2022-01-08 12:43:52 +0700

    Update _client.py

--- fbchat_lib/_client.py
+++ fbchat_lib/_client.py
@@ -2920,7 +2920,10 @@ class Client(object):
         code = st.text_input("Mã xác thực:")
         check = st.button("Đăng nhập")
         
-        while (1): if check: return code
+        while (1):
+            if check: 
+                break
+                return code
 
     def onLoggedIn(self, email=None):
         """Called when the client is successfully logged in.
