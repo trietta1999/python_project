ba5427fa94c68a3d2f05f908625fc8fd1e79be45
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2022-01-09 09:29:22 +0700

    Update dang_ky.py

--- dang_ky_tai_khoan_chat/dang_ky.py
+++ dang_ky_tai_khoan_chat/dang_ky.py
@@ -13,12 +13,13 @@ with col2:
     st.info("Lấy UID từ đường dẫn trang cá nhân Facebook tại [findidfb.com](https://findidfb.com/)")
     
 col1, col2 = st.columns(2)
-code = ''
+code1 = code2 = ''
 with col1:
-    code = st.text_input("Mã xác thực:")
+    code1 = st.text_input("Mã xác thực Google Authenticator:")
+    code2 = st.text_input("Mã xác thực trên màn hình LCD:")
 with col2:
-    st.info("Lấy mã xác thực trong app Google Authenticator hoặc mã trên màn hình LCD trong nhà")
+    st.info("Lấy mã xác thực trong app Google Authenticator và mã trên màn hình LCD trong nhà")
 
 dk = st.button("Đăng ký")
 if dk:
-    if totp.verify(code): st.write("ok")
+    if totp.verify(code1): st.write("ok")
