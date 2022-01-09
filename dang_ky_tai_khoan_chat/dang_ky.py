2c3eea2730af257f095e4cb06bdd1a031782a120
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2022-01-09 13:26:49 +0700

    Update dang_ky.py

--- dang_ky_tai_khoan_chat/dang_ky.py
+++ dang_ky_tai_khoan_chat/dang_ky.py
@@ -31,9 +31,10 @@ if dk:
         firebase.put("/", "request/uid", uid)
         firebase.put("/", "request/code", code2)
 
-if firebase.get('/request/success', None)==1:
-    st.write("Đăng ký tài khoản thành công.")
-    firebase.put("/", "request/success", 0)
-elif firebase.get('/request/success', None)==2:
-    st.write("Đăng ký tài khoản không thành công.")
-    firebase.put("/", "request/success", 0)
+# if firebase.get('/request/success', None)==1:
+#     st.write("Đăng ký tài khoản thành công.")
+#     firebase.put("/", "request/success", 0)
+# elif firebase.get('/request/success', None)==2:
+#     st.write("Đăng ký tài khoản không thành công.")
+#     firebase.put("/", "request/success", 0)
+st.write(firebase.get('/request/success', None))
