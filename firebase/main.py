254742b07b5ba72c1ee2b63014d13c44b31a98c7
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2022-11-08 21:13:33 +0700

    Update main.py

--- firebase/main.py
+++ firebase/main.py
@@ -1,7 +1,11 @@
 import streamlit as st
 from firebase import firebase
+import time, random
 
 firebase = firebase.FirebaseApplication('https://smart-steward-default-rtdb.firebaseio.com/', None)
-result = firebase.get('/cs', None)
-st.write(result)
-firebase.put("/","cs","1")
+# result = firebase.get('/cs', None)
+# st.write(result)
+a = random.randint(0,10)
+firebase.put("/","cs",a)
+time.sleep(0.5)
+st.script_runner.RerunException(st.script_request_queue.RerunData(None))
