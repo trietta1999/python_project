1d81c91f29b2f47715d2996cb4e22b9139f6adff
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2022-11-08 21:11:20 +0700

    Update main.py

--- firebase/main.py
+++ firebase/main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from firebase import firebase
 
-firebase = firebase.FirebaseApplication('smart-steward-default-rtdb.firebaseio.com', None)
+firebase = firebase.FirebaseApplication('https://smart-steward-default-rtdb.firebaseio.com/', None)
 result = firebase.get('/cs', None)
 st.write(result)
 firebase.put("/","cs","1")
