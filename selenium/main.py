d23a7d87a7bd777998c47be615aa5850a705ba47
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2022-11-13 00:14:03 +0700

    Update main.py

--- selenium/main.py
+++ selenium/main.py
@@ -3,4 +3,4 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-driver.get("https://google.com")
+# driver.get("https://google.com")
