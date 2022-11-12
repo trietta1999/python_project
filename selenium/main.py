49fd0953a2b51f2203c2404f59a19337a8a85e46
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2022-11-13 00:24:23 +0700

    Update main.py

--- selenium/main.py
+++ selenium/main.py
@@ -1,6 +1,7 @@
 # selenium 4
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
