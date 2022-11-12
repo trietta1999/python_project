d22eb2967f369c5cb5cf409090d5983d975e27e9
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2022-11-13 00:16:17 +0700

    Update main.py

--- selenium/main.py
+++ selenium/main.py
@@ -1,3 +1,6 @@
+from selenium import webdriver
+from webdriver_manager.firefox import FirefoxDriverManager
+
 from webdriver_manager.core.utils import read_version_from_cmd, PATTERN
 version = read_version_from_cmd("/usr/bin/firefox-bin --version", PATTERN["firefox"])
 driver_binary = FirefoxDriverManager(version=version).install()
