9152556a425fea9768c8ee0e07b568f0dba091cd
trietta1999 <92389789+trietta1999@users.noreply.github.com>
2022-11-13 00:18:03 +0700

    Update main.py

--- selenium/main.py
+++ selenium/main.py
@@ -1,6 +1,10 @@
+import streamlit as st
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 
 from webdriver_manager.core.utils import read_version_from_cmd, PATTERN
 version = read_version_from_cmd("/usr/bin/firefox-bin --version", PATTERN["firefox"])
 driver_binary = GeckoDriverManager(version=version).install()
+
+st.write(version)
+st.write(driver_binary)
