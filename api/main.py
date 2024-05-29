783166fbe7021237f407bb526e2da15acf145f98
Triet Ta <tadongthuyuyen@gmail.com>
2024-05-29 21:59:33 +0700

    a

--- api/main.py
+++ api/main.py
@@ -1,5 +1,16 @@
+import bottle, socket
 import streamlit as st
-import os, bottle
 
-st.write("api")
-os.system("python api/api.py")
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+
+app = bottle.app()
+
+@app.route('/data')
+def get_data():
+    return "data"
+
+# Chạy ứng dụng Bottle
+if __name__ == '__main__':
+    st.write(IPAddr)
+    app.run(port=8502)
